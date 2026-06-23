1b0307192e16df4cd40b09cf14674919bb223070
unesa-smarte <nyomanyudik@gmail.com>
2026-06-23 10:12:57 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,38 @@
+// config.js
+const DAFTAR_PERIODE_TAGIHAN = {
+    "LIDAH_WETAN": [
+        { value: "lidah_Januari_26", label: "Januari 2026 (Lidah Wetan)" },
+        { value: "lidah_Februari_26", label: "Februari 2026 (Lidah Wetan)" },
+        { value: "lidah_Maret_26", label: "Maret 2026 (Lidah Wetan)" },
+        { value: "lidah_April_26", label: "April 2026 (Lidah Wetan)" },
+        { value: "lidah_Mei_26", label: "Mei 2026 (Lidah Wetan)" },
+        { value: "lidah_Juni_26", label: "Juni 2026 (Lidah Wetan)" }
+        // Bulan depan cukup tambah di sini, contoh:
+        // { value: "lidah_juli_26", label: "Juli 2026 (Lidah Wetan)" }
+    ],
+    "KETINTANG": [
+        { value: "ktt_Januari_26", label: "Januari 2026 (Ketintang)" },
+        { value: "ktt_Februari_26", label: "Februari 2026 (Ketintang)" },
+        { value: "ktt_Maret_26", label: "Maret 2026 (Ketintang)" },
+        { value: "ktt_April_26", label: "April 2026 (Ketintang)" },
+        { value: "ktt_Mei_26", label: "Mei 2026 (Ketintang)" },
+        { value: "ktt_Juni_26", label: "Juni 2026 (Ketintang)" }
+    ],
+    "MUSTOPO": [
+        { value: "mus_Januari_26", label: "Januari 2026 (Mustopo)" },
+        { value: "mus_Februari_26", label: "Februari 2026 (Mustopo)" },
+        { value: "mus_Maret_26", label: "Maret 2026 (Mustopo)" },
+        { value: "mus_April_26", label: "April 2026 (Mustopo)" },
+        { value: "mus_Mei_26", label: "Mei 2026 (Mustopo)" },
+        { value: "mus_Juni_26", label: "Juni 2026 (Mustopo)" }
+    ],
+    "GABUNGAN": [
+        { value: "total_Januari_2026", label: "Gabungan 3 Kampus (Januari 2026)" },
+        { value: "total_Februari_2026", label: "Gabungan 3 Kampus (Februari 2026)" },
+        { value: "total_Maret_2026", label: "Gabungan 3 Kampus (Maret 2026)" },
+        { value: "total_April_2026", label: "Gabungan 3 Kampus (April 2026)" },
+        { value: "total_Mei_2026", label: "Gabungan 3 Kampus (Mei 2026)" },
+        { value: "total_Juni_2026", label: "Gabungan 3 Kampus (Juni 2026)" },
+        { value: "tahunan_2026", label: "Total Rekap Tahunan 2026" }
+    ]
+};
